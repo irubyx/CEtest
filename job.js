67da869f09ddf7533efc1f1041f1830e7f289fee
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-07-04 17:45:53 -0300

    Added some changes

--- job.js
+++ job.js
@@ -1,1 +1,22 @@
 console.log(process.argv)
+
+const arg1 = process.argv[2]
+const arg2 = process.argv[3]
+
+const { exec } = require('child_process');
+
+const script = exec('sh runJob.sh',
+    (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    });
+
+/*
+ibmcloud login <apiKey>
+ibmcloud target -g <resourceGroup>
+ibmcloud ce project select --name <projectName>
+ibmcloud ce jobrun submit --job <jobName> -a <fileName> -a <arg1> -a <arg2>
+*/
