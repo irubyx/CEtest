b424ceab327bc0299d58b3daf8d70f3bf8b76798
Sebastian Fripp <80719326+irubyx@users.noreply.github.com>
2022-07-13 12:58:32 -0300

    Delete runJob.sh

--- runJob.sh
+++
@@ -1,4 +0,0 @@
-ibmcloud login <apiKey>
-ibmcloud target -g <resourceGroup>
-ibmcloud ce project select --name <projectName>
-ibmcloud ce jobrun submit --job <jobName> -a <fileName> -a <arg1> -a <arg2>
