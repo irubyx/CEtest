e80dde0c8c5b7ce0c1fc0679b0ba481ca3f68fcf
Sebastian Fripp <irubyxrubyx@gmail.com>
2022-07-13 13:56:48 -0300

    Update job.js

--- job.js
+++ job.js
@@ -1,11 +1,31 @@
-const arg1 = process.argv[2]
-const arg2 = process.argv[3]
-const arg3 = process.argv[4]
-const arg4 = process.argv[5]
+const cantFirstClass = process.argv[2]
+const cantEconomy = process.argv[3]
+const cantChildren = process.argv[4]
+const cantHours = process.argv[5]
+
+const calculateFood = (personas, horas) => {
+    if (horas <= 6) {
+        return personas
+    } else if (horas > 6 && horas <= 12) {
+        return personas * 2
+    } else if (horas > 12 && horas <= 24) {
+        return personas * 3
+    }
+}
+
+const calculateWater = (personas, horas) => {
+    if (horas <= 6) {
+        return personas * 2
+    } else if (horas > 6 && horas <= 12) {
+        return personas * 4
+    } else if (horas > 12 && horas <= 24) {
+        return personas * 6
+    }
+}
 
 console.log("XXX")
-console.log("Cantidad de first class: " + arg1)
-console.log("Cantidad de economy: " + arg2)
-console.log("Cantidad de niños: "+ arg3)
-console.log("Horas de viaje: " + arg4)
+console.log("Menus for first class: " + calculateFood(cantFirstClass, cantHours) + ". Bottles of water (2.5L): " + calculateWater(cantFirstClass, cantHours))
+console.log("Menus for economy class: " + calculateFood(cantFirstClass, cantHours) + ". Bottles of water (2.5L): " + calculateWater(cantFirstClass, cantHours))
+console.log("Menus for children: " + cantChildren)
+console.log("Horas de viaje: " + cantHours)
 console.log("XXX")
