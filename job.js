f07925625ce84b7ff5272af1b9d5c8979e95a742
Sebastian Fripp <80719326+irubyx@users.noreply.github.com>
2022-07-13 12:55:34 -0300

    Update job.js

--- job.js
+++ job.js
@@ -4,8 +4,8 @@ const arg3 = process.argv[4]
 const arg4 = process.argv[5]
 
 console.log("XXX")
-console.log(arg1)
-console.log(arg2)
-console.log(arg3)
-console.log(arg4)
+console.log("Cantidad de first class: " + arg1)
+console.log("Cantidad de economy: " + arg2)
+console.log("Cantidad de niños: "+ arg3)
+console.log("Horas de viaje: " + arg4)
 console.log("XXX")
